294236dbef12bdbba4117506a1c8ebff0ef6e4fb
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-07-06 12:46:50 -0400

    data validation with mongoose

--- app.js
+++ app.js
@@ -5,20 +5,29 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', {
 });
 
 const fruitsSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
-  review: String,
+  name: {
+    type: String,
+    required: [true, 'ADD A NAME'],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
 });
 
 const Fruit = mongoose.model('Fruit', fruitsSchema);
 
 const fruit = new Fruit({
-  name: 'Apple',
-  rating: 7,
-  review: 'Pretty solid as a fruit.',
+  rating: 10,
+  review: 'Peaches are great.',
 });
 
-// fruit.save();
+fruit.save();
 
 const personsSchema = new mongoose.Schema({
   name: String,
@@ -34,44 +43,44 @@ const person = new Person({
 
 // person.save();
 
-const kiwi = new Fruit({
-  name: 'Kiwi',
-  score: 10,
-  review: 'The best fruit!',
+// const kiwi = new Fruit({
+//   name: 'Kiwi',
+//   score: 10,
+//   review: 'The best fruit!',
+// });
+
+// const orange = new Fruit({
+//   name: 'Orange',
+//   score: 4,
+//   review: 'Too sour for me',
+// });
+
+// const banana = new Fruit({
+//   name: 'Banana',
+//   score: 3,
+//   review: 'Weird Texture',
+// });
+
+// Fruit.insertMany([kiwi, orange, banana], (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('Successful Addition to DB');
+//   }
+// });
+
+// Fruit.find((err, fruits) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(fruits);
+//   }
+// });
+
+// Person.find((err, persons) => {
+//   err ? console.log(err) : console.log(persons);
+// });
+
+Fruit.find((err, fruits) => {
+  err ? console.log(err) : console.log(fruits.map((fruit) => fruit.name));
 });
-
-const orange = new Fruit({
-  name: 'Orange',
-  score: 4,
-  review: 'Too sour for me',
-});
-
-const banana = new Fruit({
-  name: 'Banana',
-  score: 3,
-  review: 'Weird Texture',
-});
-
-Fruit.insertMany([kiwi, orange, banana], (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Successful Addition to DB');
-  }
-});
-
-// =========================================
-// FIND ALL DOCUMENTS
-// =========================================
-
-const findDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function (err, fruits) {
-    assert.equal(err, null);
-    console.log('Found the following records');
-    console.log(fruits);
-    callback(fruits);
-  });
-};
